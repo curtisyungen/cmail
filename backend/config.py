d5798c501978a61429ccd561d149e0a4b61c25e7
unknown <curtisyungen@gmail.com>
2024-11-30 10:22:05 -0800

    delete old mbox-to-json related files

--- backend/config.py
+++ backend/config.py
@@ -4,11 +4,6 @@ import requests
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 DATA_DIR = os.path.join(ROOT_DIR, 'data')
 MODELS_DIR = os.path.join(ROOT_DIR, 'models')
-SRC_DIR = os.path.join(ROOT_DIR, 'src')
-
-MBOX_DATA = os.path.join(DATA_DIR, 'emails.mbox')
-EMAILS = os.path.join(DATA_DIR, 'emails.json')
-RAW_EMAILS = os.path.join(DATA_DIR, 'raw_emails.json')
 
 CORPUS_PATH = os.path.join(DATA_DIR, 'corpus.mm')
 DICT_PATH = os.path.join(DATA_DIR, 'dictionary.dict')
--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,5 @@
 import pandas as pd
 from src.kmeans.kmeans import run_kmeans
-from src.utils.mbox_to_json import mbox_to_json
 
 def run_kmeans_model(emails_df: pd.DataFrame, num_clusters: int = 12, categories: list[str] = [], lda_config = None):
     print(f"Running k-means with {num_clusters} clusters...")
@@ -10,7 +9,3 @@ def run_kmeans_model(emails_df: pd.DataFrame, num_clusters: int = 12, categories
         categories=categories,
         lda_config=lda_config
     )
-
-def generate_new_data(email_count: int = 100):
-    print(f"Generating data...")
-    return mbox_to_json(email_count=email_count)
